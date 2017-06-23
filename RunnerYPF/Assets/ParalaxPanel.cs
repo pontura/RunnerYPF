638439b5bb3f4986f9f37754c4dca5727b7fc7c3
pontura <pontura@gmail.com>
2017-06-23 13:59:14 -0300

    parallax mejorado

--- RunnerYPF/Assets/ParalaxPanel.cs
+++ RunnerYPF/Assets/ParalaxPanel.cs
@@ -7,14 +7,13 @@ public class ParalaxPanel : MonoBehaviour {
 	public int speed;
 	public int assetLength;
 
-	void Update () {
-		GameManager gameManager = Game.Instance.gameManager;
+	void Update () {	
 
-		if (gameManager.state != GameManager.states.PLAYING)
+		if (Game.Instance.gameManager.state != GameManager.states.PLAYING)
 			return;
 		
 		Vector3 pos = transform.localPosition;
-		pos.z -= (gameManager.realSpeed / speed ) * Time.deltaTime;	
+		pos.z -= (Game.Instance.gameManager.realSpeed / speed ) * Time.deltaTime;	
 		if (pos.z < -assetLength)
 			pos.z = 0;
 		transform.localPosition = pos;
