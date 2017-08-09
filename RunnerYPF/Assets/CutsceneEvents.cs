550060753cb546b5deac2d5679a703ef4c54642d
jpamato <juanpamato@gmail.com>
2017-08-09 19:45:55 -0300

    videos

--- RunnerYPF/Assets/CutsceneEvents.cs
+++ RunnerYPF/Assets/CutsceneEvents.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutsceneEvents : MonoBehaviour {
+
+	public bool newGameOnEnd;
+	VideoPlayer vplayer;
+	
+	void Start(){
+		
+	}
+
+	void OnEnable(){
+		vplayer = GetComponent<VideoPlayer> ();
+		vplayer.loopPointReached += EndReached;
+		vplayer.Play ();
+	}
 
 	public void OnCutsceneComplete(int newGame){
 		Events.OnCutsceneComplete (newGame==1);
@@ -10,5 +24,15 @@ public class CutsceneEvents : MonoBehaviour {
 
 	public void OnCutsceneLastScene(){
 		Events.OnCutsceneFinal ();
+	}
+
+	void EndReached(UnityEngine.Video.VideoPlayer vp)
+	{
+		Events.OnCutsceneFinal ();
+		Invoke ("CutsceneClose", 3f);
+	}
+
+	void CutsceneClose(){
+		Events.OnCutsceneComplete (newGameOnEnd);
 	}
 }
